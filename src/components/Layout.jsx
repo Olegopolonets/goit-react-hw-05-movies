598b2ca03e0a9815a5e9a45e51542655bba74e9c
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-15 18:17:04 +0200

    add styled

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Layout = () => {
   return (
     <>
-      <header>
+      <StyledHeader>
+        <h1>Scorsese</h1>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/movies">Movie</Link>
+          <StyledNavLink to="/">Home</StyledNavLink>
+          <StyledNavLink to="/movies">Movie</StyledNavLink>
         </nav>
-      </header>
+      </StyledHeader>
       <Outlet />
     </>
   );
 };
 
 export default Layout;
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 40px;
+  background-color: tomato;
+  font-size: 1.2rem;
+  nav {
+    display: flex;
+    gap: 10px;
+  }
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: black;
+  font-weight: 700;
+  &.active {
+    color: bisque;
+    text-decoration: underline;
+  }
+`;
